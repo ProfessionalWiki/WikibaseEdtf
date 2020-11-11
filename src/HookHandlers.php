eb2e429859a9ca4b20391b8aa2849c65fd1d6ea3
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-11-11 17:42:30 +0100

    Add foundation for Wikibase EDTF extension
    ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
    ░░░░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░░
    ░░░░░░░░▄▀░░░░░░░░░░░░▄░░░░░░░▀▄░░░░░░░
    ░░░░░░░░█░░▄░░░░▄░░░░░░░░░░░░░░█░░░░░░░
    ░░░░░░░░█░░░░░░░░░░░░▄█▄▄░░▄░░░█░▄▄▄░░░
    ░▄▄▄▄▄░░█░░░░░░▀░░░░▀█░░▀▄░░░░░█▀▀░██░░
    ░██▄▀██▄█░░░▄░░░░░░░██░░░░▀▀▀▀▀░░░░██░░
    ░░▀██▄▀██░░░░░░░░▀░██▀░░░░░░░░░░░░░▀██░
    ░░░░▀████░▀░░░░▄░░░██░░░▄█░░░░▄░▄█░░██░
    ░░░░░░░▀█░░░░▄░░░░░██░░░░▄░░░▄░░▄░░░██░
    ░░░░░░░▄█▄░░░░░░░░░░░▀▄░░▀▀▀▀▀▀▀▀░░▄▀░░
    ░░░░░░█▀▀█████████▀▀▀▀████████████▀░░░░
    ░░░░░░████▀░░███▀░░░░░░▀███░░▀██▀░░░░░░
    ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
    ░░░░░░ https://Professional.Wiki ░░░░░░
    ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░

---
+++ src/HookHandlers.php
@@ -0,0 +1,22 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Wikibase\EDTF;
+
+use ValueFormatters\FormatterOptions;
+use Wikibase\LocalMedia\WikibaseLocalMedia;
+
+final class HookHandlers {
+
+	public static function onExtensionRegistration(): void {
+
+	}
+
+	public static function onWikibaseRepoDataTypes( array &$dataTypeDefinitions ): void {
+	}
+
+	public static function onWikibaseClientDataTypes( array &$dataTypeDefinitions ): void {
+	}
+
+}
