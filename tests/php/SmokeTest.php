df9939c41126d02fc0242e89ddbbadfb9b4a5e64
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-11-11 18:03:42 +0100

    Add testing stub and GH Action CI

---
+++ tests/php/SmokeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Wikibase\EDTF\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SmokeTest extends TestCase {
+
+	public function testTodo() {
+		$this->assertTrue( true );
+	}
+
+}
