0787123836004aae5b14b527967bebb2c1e37d4f
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-11-12 17:39:24 +0100

    Add first formatter test

--- tests/php/Unit/EdtfFormatterTest.php
+++ tests/php/Unit/EdtfFormatterTest.php
@@ -4,12 +4,22 @@ declare( strict_types = 1 );
 
 namespace Wikibase\EDTF\Tests\Unit;
 
+use DataValues\BooleanValue;
 use PHPUnit\Framework\TestCase;
+use Wikibase\EDTF\Services\EdtfFormatter;
 
+/**
+ * @covers \Wikibase\EDTF\Services\EdtfFormatter
+ */
 class EdtfFormatterTest extends TestCase {
 
-	public function testFail() {
-		$this->assertTrue( false );
+	public function testThrowsExceptionForNonStringValues() {
+		$this->expectException( \InvalidArgumentException::class );
+		$this->newFormatter()->format( new BooleanValue( false ) );
+	}
+
+	private function newFormatter(): EdtfFormatter {
+		return new EdtfFormatter();
 	}
 
 }
