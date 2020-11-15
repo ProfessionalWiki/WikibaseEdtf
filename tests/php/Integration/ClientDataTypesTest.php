cb5f228713f3a264d23cdc2178b7796911a72ffb
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-11-15 19:12:47 +0100

    Clean client integration test

--- tests/php/Integration/ClientDataTypesTest.php
+++ tests/php/Integration/ClientDataTypesTest.php
@@ -4,21 +4,15 @@ declare( strict_types = 1 );
 
 namespace Wikibase\EDTF\Tests\Unit;
 
-use DataValues\StringValue;
 use PHPUnit\Framework\TestCase;
-use ValueFormatters\FormatterOptions;
 use Wikibase\Client\WikibaseClient;
 use Wikibase\EDTF\HookHandlers;
 use Wikibase\Lib\DataTypeDefinitions;
-use Wikibase\Lib\Formatters\SnakFormatter;
-use Wikibase\Repo\WikibaseRepo;
 
 /**
  * @covers \Wikibase\EDTF\HookHandlers
  */
 class ClientDataTypesTest extends TestCase {
-
-	private const VALID_DATE_AND_TIME = '1985-04-12T23:20:30';
 
 	public function testCanConstructDataTypeDefinitions() {
 		$definitions = new DataTypeDefinitions( $this->newTypeArray() );
