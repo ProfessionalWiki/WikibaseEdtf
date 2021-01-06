df5dc9c00fca43d22e6c6e754a2297025f836fb4
Jeroen De Dauw <jeroendedauw@gmail.com>
2021-01-06 19:23:22 +0100

    Add level 2 validity check tests

--- tests/php/ValidEdtfStrings.php
+++ tests/php/ValidEdtfStrings.php
@@ -12,6 +12,7 @@ class ValidEdtfStrings {
 	public static function allFromStandard(): \Generator {
 		yield from self::level0();
 		yield from self::level1();
+		yield from self::level2();
 	}
 
 	public static function level0(): \Generator {
@@ -65,6 +66,44 @@ class ValidEdtfStrings {
 		yield '/1985';
 
 		yield '-1985';
+	}
+
+	public static function level2(): \Generator {
+		yield 'Y-17E7';
+
+		yield '1950S2';
+		yield 'Y171010000S3';
+		yield 'Y3388E2S3';
+
+		yield '2001-34';
+
+		yield '[1667,1668,1670..1672]';
+		yield '[..1760-12-03]';
+		yield '[1760-12..]';
+		yield '[1760-01,1760-02,1760-12..]';
+		yield '[1667,1760-12]';
+		yield '[..1984]';
+
+		yield '{1667,1668,1670..1672}';
+		yield '{1960,1961-12}';
+		yield '{..1984}';
+
+		yield '2004-06-11%';
+		yield '2004-06~-11';
+		yield '2004?-06-11';
+
+		yield '?2004-06-~11';
+		yield '2004-%06-11';
+
+		yield '156X-12-25';
+		yield '15XX-12-25';
+		yield 'XXXX-12-XX';
+		yield '1XXX-XX';
+		yield '1XXX-12';
+		yield '1984-1X';
+
+		yield '2004-06-~01/2004-06-~20';
+		yield '2004-06-XX/2004-07-03';
 	}
 
 }
